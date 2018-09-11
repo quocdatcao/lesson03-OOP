3800acb1df9082cfa2566e150a789c437c9264ae
Quoc Dat <nguoitoiyeu0702@gmail.com>
2018-09-11 20:14:19 +0700

    class sinh vien

---
+++ src/dat/dev/SinhVien.java
@@ -0,0 +1,14 @@
+package dat.dev;
+
+public class SinhVien {
+	  private String Name;
+    private double diemtoan;
+    private double diemly;
+    private double diemhoa;
+    public SinhVien(String Name,double diemtoan,double diemly,double diemhoa){
+  	  this.Name=Name;
+  	  this.diemtoan=diemtoan;
+  	  this.diemly=diemly;
+  	  this.diemhoa=diemhoa;
+    }
+}
