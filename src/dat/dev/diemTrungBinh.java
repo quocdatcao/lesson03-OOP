225dcbcdb60c10157d17b585ac525a5a58a759e9
Quoc Dat <nguoitoiyeu0702@gmail.com>
2018-09-11 20:17:35 +0700

    diem trung binh

---
+++ src/dat/dev/diemTrungBinh.java
@@ -0,0 +1,12 @@
+package dat.dev;
+
+public class diemTrungBinh {
+	public double diemTrungBinh(double diemtoan,double diemly,double diemhoa) {
+		   return (diemtoan+diemly+diemhoa)/3;
+		  
+	}
+	public void show() {
+		  System.out.println(Name+":"+diemTrungBinh(diemtoan, diemly, diemhoa));
+	}
+
+}
